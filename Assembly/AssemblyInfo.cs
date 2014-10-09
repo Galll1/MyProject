f378381d8ec222db7e00553324fe84320865d90c
Galll1 <galll1.heken@gmail.com>
2014-10-09 22:18:47 +0200

    First Commit

---
+++ Assembly/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyProject")]
+[assembly: AssemblyProduct("MyProject")]
+[assembly: AssemblyCopyright("Copyright © MGladys 2014")]
+
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
